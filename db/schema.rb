3f4f2bb65c38ed00f346f805503a68bef6752d96
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2017-09-05 11:22:01 -0500

    se añade la funcionalidad de los votos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170905024201) do
+ActiveRecord::Schema.define(version: 20170905143513) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -22,8 +22,15 @@ ActiveRecord::Schema.define(version: 20170905024201) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-# Could not dump table "products" because of following StandardError
-#   Unknown type 'referens' for column 'user'
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", limit: 100
@@ -32,6 +39,15 @@ ActiveRecord::Schema.define(version: 20170905024201) do
     t.string "twitter_handle", limit: 50
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_votes_on_product_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
 end
